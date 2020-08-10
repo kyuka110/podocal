a72eea59dbac919cbac305ce5b829a13b9275144
kyu_ka110 <sousyu9kamome@gmail.com>
2020-08-10 13:38:32 +0900

    日表示のタイムラインを作成

--- src/components/right_column.vue
+++ src/components/right_column.vue
@@ -1,15 +1,18 @@
 <template>
     <div id ="right_main">
       <day_week_top/>
+      <day_week_bottom/>
     </div>
 </template>
 
 <script>
 import day_week_top from './day_week_top.vue'
+import day_week_bottom from './day_week_bottom.vue'
 export default {
   name: 'right_column',
   components: {
-    day_week_top
+    day_week_top,
+    day_week_bottom
   }
 }
 </script>
