a78f9263b4cc22daa01db58f699330ee24e4fd30
kyu_ka110 <sousyu9kamome@gmail.com>
2020-08-10 10:31:17 +0900

    create day_week_top.vue and move cal_week.vue

---
+++ src/components/day_week_top.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id ="day_week_box_top">
+    <!-- 1行目 曜日 -->
+    <span class="days_of_week">日</span>
+    <!-- 2行目 日付 -->
+    <span class="days">9</span>
+    <!-- 3行目 終日予定 -->
+    <p class="all_day">
+      <span> よてい<br>aaaaaaaa</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "day_week_top"
+}
+</script>
+
+<style scoped>
+#day_week_box_top {
+  margin: 1em;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+p {
+  width: 14.28%; /* 親要素に対して7等分する */
+  min-height: 8vh;
+}
+
+span {
+  width: 14.28%; /* 親要素に対して7等分する */
+  margin: 0.5% 0;
+}
+
+.days_of_week {
+  font-size: 13px;
+}
+
+.days {
+  font-size: 33px;
+}
+
+.all_day {
+  height: 3em;
+  background-color: aquamarine;
+}
+
+#timeline {
+  width: 100%;
+  border: solid 1px #2c3e50;
+}
+</style>
