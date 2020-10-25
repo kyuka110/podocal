b62bd3023d99e1d24fe7eeb64fbe183862295b58
Mckee <sousyu9kamome@gmail.com>
2020-10-25 23:56:16 +0900

    タスク開始時間とタイトル保持用配列を追加

--- src/store/index.js
+++ src/store/index.js
@@ -7,11 +7,12 @@ export default new Vuex.Store({
   strict: true,
   state: {
     zoom: 0,
-    dataSet: {},
     searchBeginTime:  4765100399000, //2120-12-31 23:59:59
     searchEndTime: 0,
     paintId: [],
-    dispDays: 7
+    dispDays: 7,
+    title: [],
+    first: []
   },
   mutations: {
     plusClick(state) {
@@ -21,9 +22,6 @@ export default new Vuex.Store({
       if (state.zoom > 0) {
         state.zoom = state.zoom - 1
       }
-    },
-    mutateDataSet(state, payload) {
-      state.dataSet = payload;
     },
     changeBeginTime(state, payload) {
       if (payload.newtime < state.searchBeginTime){
@@ -40,7 +38,11 @@ export default new Vuex.Store({
     },
     addPaintId(state, payload) {
       state.paintId.push(payload.id);
-    }
+    },
+    addTask(state, payload) {
+      state.title.push(payload.title);
+      state.first.push(payload.first);
+    },
   },
   actions: {
 
