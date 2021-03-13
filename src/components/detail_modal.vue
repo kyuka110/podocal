74fc364ef0c2f4748b9c20117c51ea557336fec8
kyu_ka110 <sousyu9kamome@gmail.com>
2021-03-13 16:17:26 +0900

    編集ボタンを押してテキスト入力できるようにする

--- src/components/detail_modal.vue
+++ src/components/detail_modal.vue
@@ -1,8 +1,17 @@
 <template>
   <MyModal class="detail_modal" @close="closeTaskDetailModal">
-    <p>{{dispTaskTitle(detailTaskId)}}</p>
-    <p>{{dispTaskBeginDate(detailTaskId)}} - {{dispTaskEndDate(detailTaskId)}}</p>
-    <p>{{dispTaskDetail(detailTaskId)}}</p>
+    <form>
+    <span v-if="!isEdit" @click="edit()"><font-awesome-icon icon="edit" class="fa-lg"/></span>
+    <p v-if="!isEdit">{{dispTaskTitle(detailTaskId)}}</p>
+    <input type="text" id="taskTitle" name="taskTitle" v-if="isEdit" :value="dispTaskTitle(detailTaskId)" class="inputBox">
+    <p v-if="!isEdit">{{dispTaskBeginDate(detailTaskId)}} - {{dispTaskEndDate(detailTaskId)}}</p>
+    <input type="text" id="taskBegin" name="taskBegin" v-if="isEdit" :value="dispTaskBeginDate(detailTaskId)" class="inputBox">
+    <input type="text" id="taskEnd" name="taskEnd" v-if="isEdit" :value="dispTaskEndDate(detailTaskId)" class="inputBox">
+    <p v-if="!isEdit">{{dispTaskDetail(detailTaskId)}}</p>
+    <textarea id="taskDetail" name="taskDetail" v-if="isEdit" :value="dispTaskDetail(detailTaskId)" class="inputBox"></textarea>
+    <button type="button" v-if="isEdit" :disabled="noChange">更新</button>
+    <button type="button" v-if="isEdit" @click="cancel">キャンセル</button>
+    </form>
   </MyModal>
 </template>
 
@@ -17,10 +26,19 @@ export default {
   name: 'detail_modal',
   props: {
   },
+  data () {
+    return {
+      isEdit: false,
+    }
+  },
   computed: {
     ...mapState('mm', ['detailTaskId']),
+    noChange() {
+      return !(this.isEdit);
+    }
   },
   methods: {
+    // モーダルを閉じる
     closeTaskDetailModal() {
       this.$store.commit('mm/isDispDetailModal', {isOpen: false})
     },
@@ -46,17 +64,36 @@ export default {
     dispTaskDetail(taskid){
       return this.$store.getters['mm/returnDetail'][taskid];
     },
+    edit(){
+      this.isEdit = true;
+    },
+    cancel() {
+      this.isEdit = false;
+    }
   }
 }
 </script>
 
 <style scoped>
 
-.detail_modal {
+.detail_modal p{
   text-align: left;
+}
+
+span{
+  float: right;
 }
 
 .detail_modal p{
   margin: 0.7em;
 }
+
+#taskTitle {
+  width: 100%
+}
+
+#taskDetail {
+  width: 100%
+}
+
 </style>
